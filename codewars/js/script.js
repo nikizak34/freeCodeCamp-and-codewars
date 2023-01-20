197dcf2f8c53ce218cd47eaeb262eda364d69061
nikizak34 <nikita2000.16.11@gmail.com>
2023-01-21 01:41:09 +0300

    cw2

--- codewars/js/script.js
+++ codewars/js/script.js
@@ -1,4 +1,4 @@
-//Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+//1.Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
 function evenOrOdd(number) {
    if(number % 2 === 0) {
@@ -7,6 +7,27 @@ function evenOrOdd(number) {
       return 'Odd';
     }
   
+}
+
+
+
+/*2.Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+
+*/
+
+const quarterOf = (month) => {
+  // Your code here
+  if(month<=3){
+    return 1
+  }else if(month<=6){
+    return 2
+  } else if (month <= 9) {  
+    return 3  
+  } else if (month <= 12) {  
+    return 4  
+  }
 }
 
 
