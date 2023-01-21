b317e926b0deccba79505c09b8ed7f8e8474a5f4
nikizak34 <nikita2000.16.11@gmail.com>
2023-01-21 22:15:20 +0300

    cw3

--- codewars/js/script.js
+++ codewars/js/script.js
@@ -31,3 +31,17 @@ const quarterOf = (month) => {
 }
 
 
+/* 3. Your job is simple, if x squared is more than 1000, return It's hotter than the sun!!, else, return Help yourself to a honeycomb Yorkie for the glovebox.
+
+Note: Input will either be a positive integer (or a string for untyped languages).
+
+*/
+
+function apple(x){
+if(Math.pow(x,2)>1000){
+  return "It's hotter than the sun!!"
+}else{
+ return "Help yourself to a honeycomb Yorkie for the glovebox."
+}
+  
+}
