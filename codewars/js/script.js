8727666eaa538622526f97ddd2de24c6a7b1c1b5
nikizak34 <nikita2000.16.11@gmail.com>
2023-01-22 23:11:08 +0300

    fccjs5

--- codewars/js/script.js
+++ codewars/js/script.js
@@ -49,5 +49,23 @@ function addFive(num) {
   return total
 }
 
+/* 5. It's the academic year's end, fateful moment of your school report. The averages must be calculated. All the students come to you and entreat you to calculate their average for them. Easy ! You just need to write a script.
+
+Return the average of the given array rounded down to its nearest integer.
+
+The array will never be empty.
+
+*/
+
+function getAverage(arr){
+    let sum = 0;
+ 
+    for(let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    } 
+    return Math.floor(sum / arr.length);
+
+}
+
 
 
