4f16d3c81cb8c2686e1d1cf1de8409ad2176db1e
nikizak34 <nikita2000.16.11@gmail.com>
2023-01-25 21:30:28 +0300

    cwjs9

--- codewars/js/script.js
+++ codewars/js/script.js
@@ -114,3 +114,17 @@ const areaOrPerimeter = function(l , w) {
     return (l+w)*2
   }
 };
+
+
+/* 9.Create a function that checks if a number n is divisible by two numbers x AND y. All inputs are positive, non-zero numbers. */
+
+
+function isDivisible(n, x, y) {
+  if (n % x === 0 && n % y === 0) {
+    return true;
+  }
+  return false;
+}
+
+
+
